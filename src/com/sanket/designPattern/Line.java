17fcf6cefcf57fd6cf9bc0c785e16277fe95a86b
sanket <sanketjain1234@gmail.com>
2020-07-06 00:06:16 +0530

    adapter pattern example

---
+++ src/com/sanket/designPattern/Line.java
@@ -0,0 +1,9 @@
+package com.sanket.designPattern;
+
+public class Line {
+
+    public void draw(int coordinateX1, int coordinateY1, int coordinateX2, int coordinateY2) {
+        System.out.println("Line from point A(" + coordinateX1 + ";" + coordinateY1 + "), to point B(" + coordinateX2 + ";" + coordinateY2 + ")");
+    }
+
+}
